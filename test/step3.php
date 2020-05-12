8f3c4773ac19f63af6a790145776710091b961bd
Luiz Ricardo de Lima <luizricardodelima@hotmail.com>
2020-05-12 16:16:22 -0300

    Test changes

--- test/step3.php
+++ test/step3.php
@@ -3,17 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>DynamicForm test</title>
 </head>
 <body>
 <h1>DynamicForm test</h1>
 <h2>Step 3 - Input validation and display</h2>
-<pre>
+<script>
+function toggleShow()
+{
+  var x = document.getElementById("variables_area");
+  if (x.style.display === "none") x.style.display = "block";
+  else x.style.display = "none";
+}
+</script>
+<div class="toolbar">
+<button onclick="toggleShow()">Variables</button>
+</div>
+<pre id="variables_area">
 <?php
 require_once '../custominput.class.php';
 
-var_dump($_POST);
 $customInput = new CustomInput($_POST['s'], $_POST['c']);
+
+
+var_dump($_POST);
 var_dump($customInput);
 
 ?>
